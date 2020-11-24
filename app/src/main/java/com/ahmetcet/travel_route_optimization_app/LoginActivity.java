bddb9c78ca88c7fa3566a3db7397700eaf92d451
ahmetcetinkaya <acetinkaya@geobilgi.com>
2020-11-24 23:49:22 +0300

    LoginActivity is editted and alert messsage method is implemented

--- app/src/main/java/com/ahmetcet/travel_route_optimization_app/LoginActivity.java
+++ app/src/main/java/com/ahmetcet/travel_route_optimization_app/LoginActivity.java
@@ -2,7 +2,9 @@ package com.ahmetcet.travel_route_optimization_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -19,14 +21,31 @@ public class LoginActivity extends AppCompatActivity {
 
     public void Login(View view) {
         EditText edt_loginMail = (EditText) findViewById(R.id.editText_loginEmail);
+        String input_loginMail = edt_loginMail.getText().toString();
         EditText edt_loginPassword = (EditText) findViewById(R.id.editText_loginPassword);
+        String input_loginPassword = edt_loginPassword.getText().toString();
 
-        PrefManager.putPref_UserInfo(PrefManager.key_email,edt_loginMail.getText().toString(),this);
-        PrefManager.putPref_UserInfo(PrefManager.key_password,edt_loginPassword.getText().toString(),this);
+        if(input_loginMail.length()==0){
+            edt_loginMail.setError("Eposta alanı boş olamaz");
+            return;
+        }
+        if(input_loginPassword.length()==0){
+            edt_loginPassword.setError("Şifre alanı boş olamaz");
+            return;
+        }
+
+        if(!input_loginMail.equals("ahmet") || !input_loginPassword.equals("1234")){
+            Tools.ShowErrorDialog(LoginActivity.this,"Girilen kullanıcı adı veya şifre hatalı",true);
+            return;
+        }
+
+        PrefManager.putPref_UserInfo(PrefManager.key_email,input_loginMail,this);
+        PrefManager.putPref_UserInfo(PrefManager.key_password,input_loginPassword,this);
 
         startActivity(new Intent(LoginActivity.this,AppMainActivity.class));
     }
 
     public void SignUp(View view) {
+        startActivity(new Intent(LoginActivity.this, SignupActivity.class));
     }
 }
--- app/src/main/java/com/ahmetcet/travel_route_optimization_app/Tools.java
+++ app/src/main/java/com/ahmetcet/travel_route_optimization_app/Tools.java
@@ -3,6 +3,8 @@ package com.ahmetcet.travel_route_optimization_app;
 import android.content.Context;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
+
 public class Tools {
     public static void ShowProcessDialog(Context context){
     }
@@ -17,7 +19,12 @@ public class Tools {
 
 
     public static void ShowErrorDialog(Context context, String message, boolean isCancelable){
-
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+        builder1.setMessage(message);
+        builder1.setTitle("Hata!!");
+        builder1.setCancelable(isCancelable);
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 
 
